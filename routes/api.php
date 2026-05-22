796b47b2a545f0d98934aaa6cea5a640a3adb282
Yaakoub tair <yaakoubt05@gmail.com>
2026-05-22 10:21:41 +0100

    Update API routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,37 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SourceController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Auth routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login',    [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me',      [AuthController::class, 'me']);
+});
+
+// Public routes
+Route::get('/articles',        [ArticleController::class, 'index']);
+Route::get('/articles/{slug}', [ArticleController::class, 'show']);
+Route::get('/search',          [ArticleController::class, 'search']);
+Route::get('/categories',      [CategoryController::class, 'index']);
+Route::get('/sources',         [SourceController::class, 'index']);
+
+// Admin protected routes
+Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+    Route::post('/articles',         [ArticleController::class, 'store']);
+    Route::delete('/articles/{article}', [ArticleController::class, 'destroy']);
+
+    Route::post('/categories',              [CategoryController::class, 'store']);
+    Route::put('/categories/{category}',    [CategoryController::class, 'update']);
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+
+    Route::post('/sources',           [SourceController::class, 'store']);
+    Route::put('/sources/{source}',   [SourceController::class, 'update']);
+    Route::delete('/sources/{source}',[SourceController::class, 'destroy']);
+});

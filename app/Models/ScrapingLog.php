3c38e5780dfc0c932a3d2eaf82362a18b30aaf1f
Yaakoub tair <yaakoubt05@gmail.com>
2026-05-22 10:21:05 +0100

    Update ScrapingLog model

--- app/Models/ScrapingLog.php
+++ app/Models/ScrapingLog.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class ScrapingLog extends Model
 {
     //
+    protected $fillable = [
+        'scraping_job_id', 'articles_found',
+        'articles_saved', 'error_message'
+    ];
+
+    public function job()
+    {
+        return $this->belongsTo(ScrapingJob::class, 'scraping_job_id');
+    }
 }

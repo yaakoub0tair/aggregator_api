223bd8d880d754c2aba0db7f30edca8c3569eaaa
Yaakoub tair <yaakoubt05@gmail.com>
2026-05-22 10:21:04 +0100

    Update ScrapingJob model

--- app/Models/ScrapingJob.php
+++ app/Models/ScrapingJob.php
@@ -7,4 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 class ScrapingJob extends Model
 {
     //
+    protected $fillable = [
+        'source_id', 'status',
+        'started_at', 'finished_at'
+    ];
+
+    protected $casts = [
+        'started_at' => 'datetime',
+        'finished_at' => 'datetime',
+    ];
+
+    public function source()
+    {
+        return $this->belongsTo(Source::class);
+    }
+
+    public function log()
+    {
+        return $this->hasOne(ScrapingLog::class);
+    }
 }

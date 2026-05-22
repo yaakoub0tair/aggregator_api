81c5272715e78b6d163af160ef48b1c1d368e01f
Yaakoub tair <yaakoubt05@gmail.com>
2026-05-22 10:21:05 +0100

    Update Source model

--- app/Models/Source.php
+++ app/Models/Source.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Source extends Model
 {
     //
+    protected $fillable = ['name', 'base_url', 'scraper_class', 'is_active'];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
+
+    public function scrapingJobs()
+    {
+        return $this->hasMany(ScrapingJob::class);
+    }
 }

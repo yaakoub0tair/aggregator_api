cc320201c68956f4e113a8dccdb10745621a24e8
Yaakoub tair <yaakoubt05@gmail.com>
2026-05-22 10:21:00 +0100

    Update Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     //
+    protected $fillable = ['name', 'slug'];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }

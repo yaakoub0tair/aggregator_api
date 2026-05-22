1a872c975eb9fd0c17c40964024e1f04f01c1b51
Yaakoub tair <yaakoubt05@gmail.com>
2026-05-22 10:21:00 +0100

    Update Article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     //
+
+    protected $fillable = [
+        'title', 'slug', 'summary',
+        'image_url', 'url', 'published_at',
+        'category_id', 'source_id'
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function source()
+    {
+        return $this->belongsTo(Source::class);
+    }
 }

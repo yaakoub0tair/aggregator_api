7c61b74c783df4c8765aab4e9bd017a36f34051f
Yaakoub tair <yaakoubt05@gmail.com>
2026-05-22 10:21:05 +0100

    Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,13 +9,29 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,HasApiTokens;
+    protected $fillable = ['name', 'email', 'password', 'role_id'];
+
+    protected $hidden = ['password', 'remember_token'];
+
+
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role->name === 'admin';
+    }
 
     /**
      * Get the attributes that should be cast.
